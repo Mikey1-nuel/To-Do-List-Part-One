3bae297e43f939f023c598dc4f097bcef579d74b
MesakDuduCoder <duduaoa99@gmail.com>
2023-04-13 22:05:30 +0530

    Add testing for clear completed function

--- src/modules/Testingunit2/script.test.js
+++ src/modules/Testingunit2/script.test.js
@@ -7,3 +7,18 @@ describe('Update todo list description with respective index number', () => {
     expect(tasksEditUpdateCompleted.editTaskDescription(0, 'edited')).toEqual(collection.description);
   });
 });
+
+describe("Update todo isCompleted with respective index number", () => {
+  test("todo isCompleted is true as the index number in param should be changes", () => {
+    expect(tasksEditUpdateCompleted.updateComplete(0)).toEqual(
+      collection.isCompleted
+    );
+  });
+});
+describe("delete todo list where isCompleted status is true", () => {
+  test("todo list should only show todo list with isCompleted status is false", () => {
+    expect(tasksEditUpdateCompleted.removeAllCompleted()).toEqual(
+      collection.length
+    );
+  });
+});
